4580fa92102f4648f5cc08016df61e76d58dc2b6
jlevine1294 <jlevine1294@gmail.com>
2017-04-24 20:21:52 -0500

    updated connections

--- config/env/deploying.js
+++ config/env/deploying.js
@@ -18,7 +18,7 @@ module.exports = {
    ***************************************************************************/
 
    models: {
-     connection: 'heroku_postgres'
+     connection: 'heroku_postgres',
       migrate: 'alter'
    },
 
